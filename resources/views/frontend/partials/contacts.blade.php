889b46c6d19aec02d49bcd98dd72c650f88b0f4a
padure <paduregheorghe7@gmail.com>
2023-12-02 17:22:23 +0200

    Maps

--- resources/views/frontend/partials/contacts.blade.php
+++ resources/views/frontend/partials/contacts.blade.php
@@ -1,17 +1,20 @@
 <div class="container-fluid py-5">
-    <div class="container pt-0">
+    <div class="container pt-0 contacte">
         <div class="row justify-content-center text-center">
-            <div class="col-lg-8">
+            <div class="col-lg-10">
                 <h6 class="d-inline-block bg-light text-primary text-uppercase py-1 px-2">Contacte</h6>
-                <h1 class="mb-5">Suntem Aici Pentru Dumneavoastră</h1>
-                <p>La Sauna Crimeea, suntem aici pentru a vă răspunde întrebărilor și pentru a face rezervări simple și rapide. Nu ezitați să ne contactați la numărul de telefon sau prin email. Vă așteptăm!</p>
+                <h1 class="mb-5">Suntem aici pentru Dumneavoastră</h1>
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-8 m-auto">
+            <div class="col-lg-6 m-auto">
+                <p>La Sauna Crimeea, suntem aici pentru a vă răspunde întrebărilor și pentru a face rezervări simple și rapide. Nu ezitați să ne contactați la numărul de telefon sau prin email. Vă așteptăm!</p>
                 <p>Ne găsiți la adresa: <b>mun. Chisinau str. Frumuşica 1.</b></p>
                 <p>Pentru orice întrebări sau rezervări, sunați la: <b><a href="tel:+37378953583">+37378953583</a></b></p>
                 <p>Venind la Sauna Crimeea, găsiți relaxare și revitalizare mereu la îndemână!</p>
+            </div>
+            <div class="col-lg-6">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10882.737969545173!2d28.8642275!3d47.0071677!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c97c04f9af0f11%3A0xc6eb540023ebf6d3!2zU2F1bmEgQ3JpbWVlYSwg0KHRgtCw0YDRi9C5INCa0YDRi9C8!5e0!3m2!1sro!2s!4v1701529751028!5m2!1sro!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,3 @@ Route::get('/servicii', [ServiceController::class, 'index'])->name('service.inde
 Route::get('/preturi', [PriceController::class, 'index'])->name('prices.index');
 Route::get('/galeire', [GalleryController::class, 'index'])->name('gallery.index');
 Route::get('/contacte', [ContactController::class, 'index'])->name('contacts.index');
-//Route::get('/cadrul-normativ', [FrontendController::class, 'law'])->name('law.index');
-//Route::get('/contacte', [FrontendController::class, 'contacts'])->name('contacts.index');
-//Route::post('/trimite-mesaj', [MessageController::class, 'store'])->name('contact.message');
-//Route::get('/evenimente', [FrontendController::class, 'event'])->name('event.index');
-//Route::get('/eveniment-by/{category}', [FrontendController::class, 'category'])->name('event.category');
-//Route::get('/evenimente/{title}', [FrontendController::class, 'more'])->name('event.show');
-//Route::get('/galerie', [FrontendController::class, 'galerie'])->name('galerie.index');
-//Route::get('/meniu', [FrontendController::class, 'menu'])->name('menu.index');
-//Route::get('/programul-zilei', [FrontendController::class, 'regime'])->name('regime.index');
-//Route::get('/echipa-noastra', [FrontendController::class, 'team'])->name('team.index');
